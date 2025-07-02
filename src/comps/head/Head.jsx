7cc5d0c91a0b0b53e1e2d584783bc46c60e0f4aa
Brobo1 <armanahz95@gmail.com>
2025-07-02 15:08:29 +0200

    Add navbar to `Head` component with updated styling in `App.css`

--- src/comps/head/Head.jsx
+++ src/comps/head/Head.jsx
@@ -1,7 +1,16 @@
 export function Head() {
-	return (
-		<>
-		<div><h1>DinOppgave</h1></div>
-		</>
-	)
+  return (
+    <>
+      <div id={"navbar"}>
+        <h1 id={"navbarTitle"}>DinOppgave</h1>
+        <div id={"navbarBtns"}>
+          <p>Prosjekter</p>
+          <p>For Fagfolk</p>
+          <p>Min Hjemmeside</p>
+          <p>Legg ut oppdrag</p>
+          <p>Logg inn</p>
+        </div>
+      </div>
+    </>
+  );
 }
